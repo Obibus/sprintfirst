f602accbfbb26024c4eabd753a4c58f11faa179f
Maks Bidziura <maksbidzura@gmail.com>
2023-05-08 15:00:51 +0300

    commit

---
+++ src/test/java/tests/FailtestTestCase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class FailtestTestCase {
+    @Test
+    public void checkSubmit() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://pdr.infotech.gov.ua/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+        driver.findElement(By.xpath("//button[1]")).click();
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='AuthModal_formGroup__N6_j_']//input[@name='email']")));
+        element.sendKeys("makssmaks8@gmail.com");
+        WebElement element2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input [@id='password']")));
+        element2.sendKeys("Grzn5WRB24RCkP");
+        WebElement element3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div//div//div//div//div[@class='AuthModal_btnWrap__TuT_L']//button[1]")));
+        element3.click();
+        WebElement element4 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Тести']")));
+        element4.click();
+
+        WebElement element_test = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='Tests_itemInner__jkDuo  ']")));
+        element_test.click();
+
+        WebElement element_button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='Exam_buttonWrap__aCRIM']//button")));
+        element_button.click();
+        WebElement element_button2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='Exam_buttonWrap__aCRIM']//button[text()='У разі виконання випередження колони.']")));
+        element_button2.click();
+
+
+
+    }
+}
